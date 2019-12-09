840cd0a055752c44c65626a336bee1409b12b54f
Alex Kiprop <kipropalex59@gmail.com>
2019-12-09 14:19:23 +0300

    changes

---
+++ meddy/serializer.py
@@ -0,0 +1,12 @@
+# from rest_framework import serializers
+# from .models import Procedure,Profile
+
+# class ProcedureSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Procedure
+#         fields = ('profile_picture','prof_user','bio','all_procedure')
+
+# class ProcedureSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Procedure
+#         fields = ('title','details','link','image','steps','process')
